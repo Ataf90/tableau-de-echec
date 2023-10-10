6e04048d2e0e17dacda36e98709cb47bc8e96968
Ataf_Georges <ataf_eleid@hotmail.com>
2023-10-10 17:02:53 +0200

    damier troisem modification

--- assests/script/script.js
+++ assests/script/script.js
@@ -1,18 +1,38 @@
 console.log("Script chargé !");
 let maTable = document.createElement("table");
+
 document.body.appendChild(maTable);
+// je crée ma cellule est vide
+let maCellule = " ";
 
 // Génération des lignes de ma table
-for (let lignes = 1; lignes <= 8; lignes++) {
+// crée une tableau
+let alphalist = ["A", "B", "C", "D", "E", "F", "G", "H"];
+// crée un ligne pour insert row dans la function insertRow
+
+// function doLigne(lignes, maLignes) {}
+for (let lignes = 0; lignes <= 8; lignes++) {
   let maLigne = document.createElement("tr");
   maTable.appendChild(maLigne);
   // console.log("numéro de lignes/de table : ", lignes);
-  for (let cellules = 1; cellules <= 8; cellules++) {
-    let maCellule = document.createElement("td");
+  for (let cellules = 0; cellules <= 8; cellules++) {
+    // crreate un element nommer : "td"
+    maCellule = document.createElement("td");
     // maCellule.textContent = (cellules * lignes).toString();
-    (cellules + lignes + 1) % 2 === 0
-      ? (maCellule.style.backgroundColor = "white")
-      : (maCellule.style.backgroundColor = "black");
+    if (cellules == 0 && lignes >= 1) {
+      maCellule.textContent = lignes;
+    } else {
+      (lignes % 2 == 1 && cellules % 2 == 1) ||
+      (lignes % 2 == 0 && cellules % 2 == 0)
+        ? (maCellule.style.backgroundColor = "black")
+        : (maCellule.style.backgroundColor = "white");
+    }
+    if (lignes == 0) maCellule.style.backgroundColor = "";
+    // suprimer le column
+    //
+    if (lignes == 0 && cellules >= 1) {
+      maCellule.textContent = alphalist[cellules - 1];
+    }
     maLigne.appendChild(maCellule);
     // console.log("numéro de cellule/multiplicateur : ", lignes);
   }
