dbaabd62d8c9f98880ad187ca75ec34945d59f1f
Ataf_Georges <ataf_eleid@hotmail.com>
2023-10-10 22:29:35 +0200

    Factoriser mon code de JS

--- assests/script/script.js
+++ assests/script/script.js
@@ -3,13 +3,12 @@ let maTable = document.createElement("table");
 
 document.body.appendChild(maTable);
 // je crée ma cellule est vide
-let maCellule = " ";
+let maCellule = "";
 
-// Génération des lignes de ma table
 // crée une tableau qui compris tous les characters
-let alphalist = ["A", "B", "C", "D", "E", "F", "G", "H"];
+let alphaList = ["A", "B", "C", "D", "E", "F", "G", "H"];
 
-function doLigne(lignes, maLignes) {}
+// function doLigne(lignes, maLignes) {}
 for (let lignes = 0; lignes <= 8; lignes++) {
   let maLigne = document.createElement("tr");
   maTable.appendChild(maLigne);
@@ -22,17 +21,15 @@ for (let lignes = 0; lignes <= 8; lignes++) {
     if (cellules == 0 && lignes >= 1) {
       maCellule.textContent = lignes;
     } else {
-      (lignes % 2 == 1 && cellules % 2 == 1) ||
-      (lignes % 2 == 0 && cellules % 2 == 0)
+      (lignes % 2 == 1 && cellules % 2 == 1) || // impaire
+      (lignes % 2 == 0 && cellules % 2 == 0) // paire
         ? (maCellule.style.backgroundColor = "black")
         : (maCellule.style.backgroundColor = "white");
     }
-    if (lignes == 0) maCellule.style.backgroundColor = "";
-    // suprimer le column
-    //
-    //pour ajouter alphapete dans mon table table
-    if (lignes == 0 && cellules >= 1) {
-      maCellule.textContent = alphalist[cellules - 1];
+    //pour ajouter alphapete dans mon table table et suprimer le column
+    if ((lignes == 0 && cellules >= 1) || lignes == 0) {
+      maCellule.style.backgroundColor = "";
+      maCellule.textContent = alphaList[cellules - 1];
     }
     maLigne.appendChild(maCellule);
     // console.log("numéro de cellule/multiplicateur : ", lignes);
