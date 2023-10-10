2fc56933dc97acce52ecd8702850eef4556d61c4
Ataf_Georges <ataf_eleid@hotmail.com>
2023-10-10 14:34:53 +0200

    Damier deuxilem midification

--- assests/script/script.js
+++ assests/script/script.js
@@ -3,14 +3,17 @@ let maTable = document.createElement("table");
 document.body.appendChild(maTable);
 
 // Génération des lignes de ma table
-for (let lignes = 1; lignes <= 10; lignes++) {
+for (let lignes = 1; lignes <= 8; lignes++) {
   let maLigne = document.createElement("tr");
   maTable.appendChild(maLigne);
-  console.log("numéro de lignes/de table : ", lignes);
-  for (let cellules = 1; cellules <= 10; cellules++) {
+  // console.log("numéro de lignes/de table : ", lignes);
+  for (let cellules = 1; cellules <= 8; cellules++) {
     let maCellule = document.createElement("td");
-    maCellule.textContent = (cellules * lignes).toString();
+    // maCellule.textContent = (cellules * lignes).toString();
+    (cellules + lignes + 1) % 2 === 0
+      ? (maCellule.style.backgroundColor = "white")
+      : (maCellule.style.backgroundColor = "black");
     maLigne.appendChild(maCellule);
-    console.log("numéro de cellule/multiplicateur : ", lignes);
+    // console.log("numéro de cellule/multiplicateur : ", lignes);
   }
 }
